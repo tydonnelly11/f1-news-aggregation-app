0f7031642eab17801c053651218852ee0131ea0c
ty d <tydonnelly11@gmail.com>
2024-03-29 15:19:39 -0500

    removed unused imports

--- f1-news-app/src/App.tsx
+++ f1-news-app/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from 'axios'
+
 import { Post } from './components/Post'
 import './App.css'
 
