b8fbcd2887c059dfc2a6fb7d7cd6d669593fd038
ty d <tydonnelly11@gmail.com>
2024-04-01 18:00:51 -0500

    updated backend

--- backend/api/posts.js
+++ backend/api/posts.js
@@ -2,13 +2,14 @@
 import { sql } from '@vercel/postgres';
 
 
-export default async function getDataFromDatabase() {
+export default async function getDataFromDatabase(req, res) {
     try {
       const  rows = await sql`SELECT * FROM f1_posts_summary_1 ORDER BY date_column DESC`;
       console.log(rows)
       
-      return rows;
     } catch (error) {
       console.error("Failed to fetch posts from database", error);
     }
+
+    res.send(rows);
   }
