0523bed7e834496cd0ce7e84d3e369fecaa5743e
ty d <tydonnelly11@gmail.com>
2024-04-01 18:05:12 -0500

    updated backend to send correct data

--- backend/api/posts.js
+++ backend/api/posts.js
@@ -6,7 +6,7 @@ export default async function getDataFromDatabase(req, res) {
     try {
       const  rows = await sql`SELECT * FROM f1_posts_summary_1 ORDER BY date_column DESC`;
       console.log(rows)
-      res.send(rows);
+      res.send(rows.rows);
       
     } catch (error) {
       console.error("Failed to fetch posts from database", error);
