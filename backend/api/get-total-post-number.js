2146156d942a933524d9a14714e7bb843c8153ca
ty d <tydonnelly11@gmail.com>
2024-04-03 11:53:31 -0500

    removed un needed express.js since i am using vercel functions and added sql for getting total number of saved posts

--- backend/api/get-total-post-number.js
+++ backend/api/get-total-post-number.js
@@ -1,12 +1,11 @@
-
 import { sql } from '@vercel/postgres';
 
 
 export default async function getDataFromDatabase(req, res) {
     try {
       const  rows = await sql`SELECT * FROM f1_posts_summary_1 ORDER BY date_column DESC`;
-      console.log(rows)
-      res.send(rows.rows, rows.rowCount);
+      
+      res.send(rows.rowCount);
       
     } catch (error) {
       console.error("Failed to fetch posts from database", error);
