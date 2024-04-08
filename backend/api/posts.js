e2ebe887b4dc810d7928497982e8a17c38e25eb4
ty d <tydonnelly11@gmail.com>
2024-04-08 04:54:54 -0500

    query working but no data is being returned

--- backend/api/posts.js
+++ backend/api/posts.js
@@ -5,6 +5,10 @@ import { sql } from '@vercel/postgres';
 export default async function getDataFromDatabase(req, res) {
     const startDate = req.query.start
     const endDate = req.query.end
+    console.log(`SELECT *
+    FROM f1_posts_summary_1
+    WHERE date_column >= ${startDate}
+    AND date_column <= ${endDate};`)
     try {
       const  rows = await sql`SELECT *
       FROM f1_posts_summary_1
